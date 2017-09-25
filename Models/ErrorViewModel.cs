039b52613220c1630f86aa808dd91957c659a18e
Draconic-saint <jkeirsmaekers@gmail.com>
2017-09-25 21:56:12 +0200

    1.1 -> 2.0 fix

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnetcore_keirsmaekers_joris.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
