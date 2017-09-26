04bb2aa90ec48d2f234c47c6ea683d3b14262f4f
Raf Ceuls <rcl@foreach.be>
2017-09-26 13:54:57 +0200

    Added models to book list instead of list of strings

--- Bibliotheek/Controllers/BookController.cs
+++ Bibliotheek/Controllers/BookController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Bibliotheek.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,11 +12,11 @@ namespace Bibliotheek.Controllers
         public IActionResult Index()
         {
             var model = new BookListViewModel();
-            model.Books = new List<string>();
-            model.Books.Add("book 1");
-            model.Books.Add("book 2");
-            model.Books.Add("book 3");
-            model.Books.Add("book 4");
+            model.Books = new List<BookDetailViewModel>();
+            model.Books.Add(new BookDetailViewModel(){Author = "author 1", CreationDate = DateTime.Now, ISBN = "isbn 1", Title = "title 1"});
+            model.Books.Add(new BookDetailViewModel(){Author = "author 2", CreationDate = DateTime.Now, ISBN = "isbn 2", Title = "title 2"});
+            model.Books.Add(new BookDetailViewModel(){Author = "author 3", CreationDate = DateTime.Now, ISBN = "isbn 3", Title = "title 3"});
+            model.Books.Add(new BookDetailViewModel(){Author = "author 4", CreationDate = DateTime.Now, ISBN = "isbn 4", Title = "title 4"});
             return View(model);
         }
 
--- Bibliotheek/Models/BookListViewModel.cs
+++ Bibliotheek/Models/BookListViewModel.cs
@@ -5,7 +5,15 @@ namespace Bibliotheek.Models
 {
     public class BookListViewModel
     {
-        public List<string> Books { get; set; }
+        public List<BookDetailViewModel> Books { get; set; }
         public DateTime GeneratedAt => DateTime.Now;
+    }
+
+    public class BookDetailViewModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public DateTime CreationDate { get; set; }
     }
 }
